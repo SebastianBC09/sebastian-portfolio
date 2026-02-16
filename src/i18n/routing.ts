a3dc72c144de67627b95c673fa130158a85d3693
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2026-02-15 21:39:53 -0500

    fixes

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,11 +1,6 @@
 import { defineRouting } from 'next-intl/routing';
-import { createNavigation } from 'next-intl/navigation';
 
 export const routing = defineRouting({
   locales: ['en', 'es'],
   defaultLocale: 'en',
 });
-
-export type Locale = (typeof routing.locales)[number];
-
-export const { Link, usePathname, useRouter, getPathName } = createNavigation(routing);
