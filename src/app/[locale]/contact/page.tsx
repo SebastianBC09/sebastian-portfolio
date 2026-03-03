2eedd964628eeacffd6c9e995f4b479b3789914a
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2026-03-02 22:34:11 -0500

    minor change

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -2,6 +2,8 @@ import { setRequestLocale } from 'next-intl/server';
 import { ContactBridge } from './_components/ContactBridge';
 import { ContactOptions } from './_components/ContactOptions';
 
+export const dynamic = 'force-dynamic';
+
 export default async function ContactPage({ params }: { params: Promise<{ locale: string }> }) {
   const { locale } = await params;
   setRequestLocale(locale);
