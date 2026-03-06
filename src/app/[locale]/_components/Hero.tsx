0c3377403d6ec217581af93481896b68162dfb0d
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2026-03-06 01:32:47 -0500

    minor change

--- src/app/[locale]/_components/Hero.tsx
+++ src/app/[locale]/_components/Hero.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { useTranslations } from 'next-intl';
+import { Link } from '@/i18n/navigation';
 import { motion } from 'framer-motion';
 import { Button } from '@/components/ui/Button';
-import { Link } from '@/i18n/navigation';
 import { HeroVisual } from './HeroVisual';
 
 const stagger = {
